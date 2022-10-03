f8de80ee400a0b6ab76874d27f82357bb50a8c5c
NaliaGK <naliagk.hulu@gmail.com>
2022-10-03 21:33:53 +0700

    cape BOI

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+ 
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+ 
+class UserController extends Controller
+{
+  
+    public function index()
+    {
+        $pedulins = Pedulin::all();
+      return view ('index')->with('pedulins', $pedulins);
+    }
+ 
+    
+    public function create()
+    {
+        return view('create');
+    }
+ 
+  
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        Pedulin::create($input);
+        return redirect('pedulin')->with('flash_message', 'User Addedd!');  
+    }
+ 
+    
+    public function show($id)
+    {
+        $pedulin = Pedulin::find($id);
+        return view('show')->with('pedulins', $pedulin);
+    }
+ 
+    
+    public function edit($id)
+    {
+        $pedulin = Pedulin::find($id);
+        return view('edit')->with('pedulins', $pedulin);
+    }
+ 
+  
+    public function update(Request $request, $id)
+    {
+        $pedulin = Pedulin::find($id);
+        $input = $request->all();
+        $pedulin->update($input);
+        return redirect('pedulin')->with('flash_message', 'User Updated!');  
+    }
+ 
+  
+    public function destroy($id)
+    {
+        Pedulin::destroy($id);
+        return redirect('pedulin')->with('flash_message', 'User deleted!');  
+    }
+}
